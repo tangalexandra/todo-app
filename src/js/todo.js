26becc47c5749596a0cb7f33a1be53133969c2de
Alexandra Tang <alexandra.tang@appliedtechnology.se>
2019-06-24 18:54:08 +0200

    Refactored - Removed reload(), the page is displaying the new contents by emptying and re-setting the innerHTML of the to-do list with the new state, after each state change

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,50 +1,58 @@
-function addNotesToHTML(HTMLelement, notes) {
-    notes.forEach((note, index) => {
-      const newToDo = document.createElement('li');
-      let html = `<h3 class="underline">${note.title}</h3><p>${note.description}</p>`;
-      if (note.done) {
-        newToDo.classList.add('done');
-        html += `<button class="btn btn-remove" type="button" id=${index}>Remove</button>`;
-      } else {
-        newToDo.classList.remove('done');
-        html += `<button class="btn btn-remove" style="display:none" type="button" id=${index}>Remove</button>`;
-      }
-      newToDo.innerHTML = html;
-      HTMLelement.appendChild(newToDo);
-    });
+function render(state) {
+  const ulToDo = document.querySelector('#todo-list');
+  while (ulToDo.firstChild) {
+    ulToDo.removeChild(ulToDo.firstChild);
   }
-  
-  function updateLocalStorage(storageObject) {
-    localStorage.setItem('toDoListObject', JSON.stringify(storageObject));
-    document.location.reload();
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('#note');
-    const myLocalStorage = localStorage.getItem('toDoListObject');
-    const myObj = myLocalStorage ? JSON.parse(myLocalStorage) : { toDoList: [] };
-    const toDoListArray = myObj.toDoList;
-    const ulToDo = document.querySelector('#todo-list');
-    addNotesToHTML(ulToDo, toDoListArray);
-  
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const inputs = form.querySelectorAll('input');
-      toDoListArray.unshift({ title: inputs[0].value, description: inputs[1].value, done: false });
-      inputs[0].value = '';
-      inputs[1].value = '';
-      updateLocalStorage(myObj);
-    });
-  
-    ulToDo.addEventListener('click', (e) => {
-      if (e.target.tagName === 'BUTTON') {
-        const btn = e.target;
-        toDoListArray.splice(btn.id, 1);
-      } else if (e.target && e.target.nodeName === 'LI') {
-        const btn = e.target.querySelector('button');
-        toDoListArray[btn.id].done = toDoListArray[btn.id].done ? false : true;
-      }
-      updateLocalStorage(myObj);
-    });
+  state.forEach((note, index) => {
+    const newToDo = document.createElement('li');
+    newToDo.setAttribute('id', index);
+    let html = `<h3 id="${index}" class="underline card">${note.title}</h3><p id="${index}" class="card">${note.description}</p>`;
+    if (note.done) {
+      newToDo.classList.add('done');
+      html += `<button class="btn btn-remove" type="button" id=${index}>Remove</button>`;
+    } else {
+      newToDo.classList.remove('done');
+      html += `<button class="btn btn-remove" style="display:none" type="button" id=${index}>Remove</button>`;
+    }
+    newToDo.innerHTML = html;
+    ulToDo.appendChild(newToDo);
   });
-  
+}
+
+function updateLocalStorage(storageObject) {
+  localStorage.setItem('toDoListObject', JSON.stringify(storageObject));
+}
+
+function getState() {
+  const myLocalStorage = localStorage.getItem('toDoListObject');
+  const myObj = myLocalStorage ? JSON.parse(myLocalStorage) : { toDoList: [] };
+  return myObj.toDoList;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#note');
+  const ulToDo = document.querySelector('#todo-list');
+  const state = getState();
+  render(state);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputs = form.querySelectorAll('input');
+    state.unshift({ title: inputs[0].value, description: inputs[1].value, done: false });
+    inputs[0].value = '';
+    inputs[1].value = '';
+    updateLocalStorage({ toDoList: state });
+    render(state);
+  });
+
+  ulToDo.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-remove')) {
+      const btn = e.target;
+      state.splice(btn.id, 1);
+    } else if (e.target.nodeName === 'LI' || e.target.classList.contains('card')) {
+      state[e.target.id].done = !state[e.target.id].done;
+    }
+    updateLocalStorage({ toDoList: state });
+    render(state);
+  });
+});
